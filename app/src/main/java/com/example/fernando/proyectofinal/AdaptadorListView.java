15d10e1b364372b35b6bc03d83e8b5f9818fffae
Fernando <jfjfv@hotmail.com>
2018-11-02 12:23:16 -0500

    DaoNota Terminado

--- app/src/main/java/com/example/fernando/proyectofinal/AdaptadorListView.java
+++ app/src/main/java/com/example/fernando/proyectofinal/AdaptadorListView.java
@@ -1,18 +1,24 @@
 package com.example.fernando.proyectofinal;
 
         import android.content.Context;
+        import android.content.Intent;
         import android.os.Build;
         import android.support.annotation.RequiresApi;
+        import android.util.Log;
         import android.view.LayoutInflater;
         import android.view.View;
         import android.view.ViewGroup;
         import android.widget.BaseAdapter;
+        import android.widget.ImageButton;
         import android.widget.ImageView;
+        import android.widget.ListView;
         import android.widget.TextView;
         import android.widget.Toast;
 
+        import com.example.fernando.proyectofinal.bd.DAOnota;
         import com.example.fernando.proyectofinal.bd.Nota;
 
+        import java.util.ArrayList;
         import java.util.List;
 
 public class AdaptadorListView extends BaseAdapter {
@@ -44,11 +50,13 @@ public class AdaptadorListView extends BaseAdapter {
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         View vista = view;
-
+        final int j = i;
         LayoutInflater inflater = LayoutInflater.from(context);
         vista = inflater.inflate(R.layout.item_list_view,null);
         ImageView iv = vista.findViewById(R.id.img_icon);
         TextView txt = vista.findViewById(R.id.txtT);
+        ImageButton imgBtnEliminar = vista.findViewById(R.id.imgbtneliminar);
+        ImageButton imgBtnEditar = vista.findViewById(R.id.imgbtneditar);
         txt.setText(listaDatos.get(i).getTitulo());
         try {
             if (listaDatos.get(i).isRecordatorio()) {
@@ -59,7 +67,33 @@ public class AdaptadorListView extends BaseAdapter {
         }catch (Exception e){
             Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
-
+        vista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),VerNota.class);
+                context.startActivity(intent);
+                Toast.makeText(view.getContext(), (listaDatos.get(j).getTitulo()), Toast.LENGTH_SHORT).show();
+            }
+        });
+        imgBtnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DAOnota dnota = new DAOnota(view.getContext());
+                dnota.delete(listaDatos.get(j).getId());
+                List<Nota> list = dnota.getAll();
+                AdaptadorListView miAdapter = new AdaptadorListView(view.getContext(), list);
+                Class_list_note.lv.setAdapter(miAdapter);
+                Toast.makeText(view.getContext(), "Eliminar", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imgBtnEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),EditarNota.class);
+                context.startActivity(intent);
+                Toast.makeText(view.getContext(), "Editar", Toast.LENGTH_SHORT).show();
+            }
+        });
         return vista;
     }
 }
